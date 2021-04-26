f24cc9876f563db6dcfe7f0efb5fd2961650ceb6
Olaia Segovia <olaia.segovia@gmail.com>
2021-04-27 00:14:56 +0200

    Revert "addUser refactored to use flex layout."
    This reverts commit f3abe45f0dce4f7450c6a2b6c433669bdce5572b.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExpenseFormComponent } from './components/expense-form/expense-form.component';
 import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
+  { path: 'adduser', component: UserFormComponent },
   { path: 'addexpense', component: ExpenseFormComponent },
-  { path: 'listexpenses', component: ExpenseListComponent },
-  { path: 'adduser', loadChildren: () => import('./components/adduser/adduser.module').then(m => m.AdduserModule) },
+  { path: 'listexpenses', component: ExpenseListComponent }
 ];
 
 @NgModule({
--- src/app/components/user-form/user-form.component.spec.ts
+++ src/app/components/user-form/user-form.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AdduserComponent } from './adduser.component';
+import { UserFormComponent } from './user-form.component';
 
-describe('AdduserComponent', () => {
-  let component: AdduserComponent;
-  let fixture: ComponentFixture<AdduserComponent>;
+describe('UserFormComponent', () => {
+  let component: UserFormComponent;
+  let fixture: ComponentFixture<UserFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdduserComponent ]
+      declarations: [ UserFormComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AdduserComponent);
+    fixture = TestBed.createComponent(UserFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
